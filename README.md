cfb86d1fe8a0dd2f75d264293029c76d1d515304
Calvin Echols <calvin.echols@gmail.com>
2021-04-19 21:57:35 -0400

    adding module level docs and a readme file with link do go docs on pkg.go.dev

---
+++ README.md
@@ -0,0 +1,1 @@
+# Docs are hosted on [pkg.go.dev](https://pkg.go.dev/github.com/calvine/simplevalidation)
---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+	This module contains packages that make validating data easy peasy.
+
+	The validation package contains the logic for validating various structures of data, and the validator package contains specific validators for data.
+
+	For more information on the validation or validator packages please see their respective package documentation.
+*/
+package simplevalidation
